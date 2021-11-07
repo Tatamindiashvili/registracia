20e849f63ef53331c9da05558c4d422d0ef9f146
Tatamindiashvili <tamar.mindiashvili.1@btu.edu.ge>
2021-11-07 09:57:31 +0400

    Initial commit

---
+++ app/src/main/java/com/example/registracia/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.registracia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var registracia: Button
+    private lateinit var saxeli: EditText
+    private lateinit var gvari: EditText
+    private lateinit var telefonisnomeri: EditText
+    private lateinit var piradinomeri: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+    }
+}
